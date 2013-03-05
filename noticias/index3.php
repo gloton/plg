0aae50dd33b55d06e2b3e34ad3a7245452d9decb
Gloton <gloton@gmail.com>
2013-03-05 15:43:11 -0300

    Tarea 3 ok y parte de la 4 Estoy creando las noticias en forma manual

---
+++ noticias/index3.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<?php
+
+if (is_file("../lib/general.php")) {
+	include_once("../lib/constantes.php");
+	include_once("../lib/general.php");
+} else {
+	include_once("lib/constantes.php");
+	include_once("lib/general.php");
+}
+	$selected = "NOSOTROS";
+?>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>PLANTILLA « Plataforma Group</title>
+<link type="text/css" rel="stylesheet" href="../css/styles.css" />
+<link type="text/css" rel="stylesheet" href="../css/blog/styles.css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<style type="text/css">
+/*post*/
+#wrapperBlog .post {
+	width: 668px;
+	height: 180px;
+	margin: 10px auto;
+}
+#wrapperBlog .postSup {
+	height: 142px;
+}
+#wrapperBlog .postInf {
+	height: 38px;
+	float: right;
+	padding-right: 20px;
+	font-weight: bold;
+}
+#wrapperBlog .postInf a {
+	color: #1F9D48;
+}
+#wrapperBlog .introImg {
+	width: 218px;
+	height: 142px;;
+	float: left;
+}
+#wrapperBlog .introText {
+	width: 450px;
+	height: 100%;
+	float: left;
+}
+/*side right*/
+</style>
+
+<style type="text/css">
+/*marca menu pp pagina seleccionada*/
+#li_noticias a {
+    background: none repeat scroll 0 0 #f0f0f0;
+    color: #000000;
+    opacity: 1;
+}
+</style>
+</head>
+<body>
+<div id="wrap">
+	<?php include_once("../partes/header.php"); ?>
+    <div class="contenedor">
+		<!-- Begin wrapperBlog -->
+		<div id="wrapperBlog">
+			<!-- Begin Left Column -->
+			<div id="leftcolumn">
+				  	<div class="post">
+		  		<div class="postSup">
+		  			<div class="introImg">
+		  				<div class="wrapIntroImg" style="box-shadow: 0 1px 5px rgba(0, 0, 0, 0.25);padding: 4px;background-color: #FFFFFF; width: 190px; height: 120px;">
+		  					<img class="introThum" src="../images/blog/noticias-3.jpg">
+		  				</div>
+		  			</div>
+		  			<div class="introText">
+		  				<div class="postTitle">
+		  					<h2>tincidunt neque inceptos 32</h2>
+		  				</div>
+		  				<div class="postDate">
+		  					<span>Lunes 01 de Octubre de 2012</span>
+		  				</div>
+		  				<div class="postCategory">
+		  							  					<span>Novedades</span>
+		  				</div>
+		  				<div class="postDetail">
+		  					<p>Adipiscing mi gravida at platea hendrerit sem suscipit nam interdum venenatis inceptos donec potenti   						
+		  					</p>
+		  				</div>  				  				  				
+		  			</div>
+		  		</div>
+		  		<div class="postInf">
+		    		
+		  			<a href="tincidunt-neque-inceptos-32-p32.html">
+		  				<span class="postMore">
+		  					Leer mas...
+		  				</span>
+		  			</a>
+		  		</div>
+		  	</div>
+				  	<div class="post">
+		  		<div class="postSup">
+		  			<div class="introImg">
+		  				<div class="wrapIntroImg" style="box-shadow: 0 1px 5px rgba(0, 0, 0, 0.25);padding: 4px;background-color: #FFFFFF; width: 190px; height: 120px;">
+		  					<img class="introThum" src="../images/blog/noticias-1.jpg">
+		  				</div>
+		  			</div>
+		  			<div class="introText">
+		  				<div class="postTitle">
+		  					<h2>tincidunt neque inceptos 30</h2>
+		  				</div>
+		  				<div class="postDate">
+		  					<span>Lunes 03 de Septiembre de 2012</span>
+		  				</div>
+		  				<div class="postCategory">
+		  							  					<span>Novedades</span>
+		  				</div>
+		  				<div class="postDetail">
+		  					<p>Adipiscing mi gravida at platea hendrerit sem suscipit nam interdum venenatis inceptos donec potenti   						
+		  					</p>
+		  				</div>  				  				  				
+		  			</div>
+		  		</div>
+		  		<div class="postInf">
+		    		
+		  			<a href="tincidunt-neque-inceptos-30-p30.html">
+		  				<span class="postMore">
+		  					Leer mas...
+		  				</span>
+		  			</a>
+		  		</div>
+		  	</div>
+				  	<div class="post">
+		  		<div class="postSup">
+		  			<div class="introImg">
+		  				<div class="wrapIntroImg" style="box-shadow: 0 1px 5px rgba(0, 0, 0, 0.25);padding: 4px;background-color: #FFFFFF; width: 190px; height: 120px;">
+		  					<img class="introThum" src="../images/blog/default.jpg">
+		  				</div>
+		  			</div>
+		  			<div class="introText">
+		  				<div class="postTitle">
+		  					<h2>tincidunt neque inceptos 22</h2>
+		  				</div>
+		  				<div class="postDate">
+		  					<span>Viernes 11 de Mayo de 2012</span>
+		  				</div>
+		  				<div class="postCategory">
+		  							  					<span>Novedades</span>
+		  				</div>
+		  				<div class="postDetail">
+		  					<p>Adipiscing mi gravida at platea hendrerit sem suscipit nam interdum venenatis inceptos donec potenti   						
+		  					</p>
+		  				</div>  				  				  				
+		  			</div>
+		  		</div>
+		  		<div class="postInf">
+		    		
+		  			<a href="tincidunt-neque-inceptos-22-p22.html">
+		  				<span class="postMore">
+		  					Leer mas...
+		  				</span>
+		  			</a>
+		  		</div>
+		  	</div>
+					</div>
+			<!-- End Left Column -->
+			<!-- Begin Right Column -->
+			<div id="rightcolumn">
+				<div id="noticias-right">
+					<img src="../images/ipad-relleno.jpg">
+				</div>			
+				<div class="postModulo">
+					<div class="postModuloTitle">
+						<h3>Categorias</h3>
+					</div>
+					<div class="postModuloCategory">
+						<ul>
+						
+							<li><a href="?cat=8"><span>ANB</span></a></li>
+						
+							<li><a href="?cat=7"><span>Clientes</span></a></li>
+						
+							<li><a href="?cat=3"><span>IMAD</span></a></li>
+						
+							<li><a href="?cat=1"><span>Novedades</span></a></li>
+						
+							<li><a href="?cat=4"><span>PCJ</span></a></li>
+						
+							<li><a href="?cat=6"><span>Plataforma de Remates</span></a></li>
+						
+							<li><a href="?cat=2"><span>Servicetonic</span></a></li>
+						
+							<li><a href="?cat=5"><span>Tecnología</span></a></li>
+							
+						</ul>
+					</div>			
+				</div>
+			</div><!-- End Right Column -->
+			<div class="clearfix"></div>
+			<div id="footerBlog">
+				<div id="paginacion">
+					<p>
+								<span>Anterior</span>
+						
+							
+		| 
+								<a href="?pos=3&amp;cat=1" title="Siguiente">Siguiente</a>
+						</p></div>
+			</div>
+		</div><!-- End wrapperBlog -->    
+    </div>
+	<div class="barra-bottom">
+		<div id="footer">
+			<ul class="listafooter">
+				<li class="titulista"><span>Plataforma Group</span></li>
+				<li>Monjitas #527, Of. 1316</li>
+				<li>Santiago </li>
+				<li>Tel. (562) 366 0130 </li>
+				<li>contacto@platafomagoup.cl</li>
+			</ul>
+			<ul class="listafooter">
+				<li class="titulista"><span>Empresa</span></li>
+				<li>Equipo</li>
+				<li>Trabaja con Nosotros</li>
+				<li>&nbsp;</li>
+			</ul>
+			<ul class="listafooter">
+				<li class="titulista"><span>Productos</span></li>
+				<li>Soluciones</li>
+				<li>Productos</li>
+				<li>Consultoría</li>
+			</ul>                
+		</div>
+		<div id="post-footer">
+        Copyright © Prueba, Privacy Statement Terms and Conditions.
+        </div>
+	</div>
+	<?php include_once("../partes/footer.php"); ?>
+</div>
+</body>
+</html>
